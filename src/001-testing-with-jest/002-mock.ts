628413971ff0c297bfb93d4aa0b1797169bdbe70
Nguyen Tang Bao An <baoan.nguyentang@foodycorp.vn>
2020-07-15 17:17:23 +0700

    Done 001

--- src/001-testing-with-jest/002-mock.ts
+++ src/001-testing-with-jest/002-mock.ts
@@ -1,4 +1,6 @@
-export const ensureNetwork = async <T>(api: Promise<T>, timeout: number, fallbackValue: T): Promise<T> => {
-  const timeoutPromise = new Promise<T>(resolve => setTimeout(() => resolve(fallbackValue), timeout))
-  return await Promise.race([api, timeoutPromise])
-}
+export const asyncSetTimeout = async <T>(callback: () => T, timeout?: number): Promise<T> => {
+  return new Promise(resolve => setTimeout(
+    () => { resolve(callback()); }, 
+    timeout
+  ));
+};
