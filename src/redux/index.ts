3aac63e4b58c842707147bcf7231f449558c99ee
ntban15 <ngtab.an.9@gmail.com>
2020-07-19 12:22:42 +0700

    Done testing-react

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,9 +2,6 @@ import { createStore } from "redux";
 
 import todosReducer from "./todosReducer";
 
-const store = createStore(
-  todosReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(todosReducer);
 
 export default store;
